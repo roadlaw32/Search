84db5d7a9c19a6f5ec7e8d652836978e3e5c1b96
roadlaw32 <106253456+roadlaw32@users.noreply.github.com>
2022-05-29 01:21:40 +0900

    Update Search.java

--- Search.java
+++ Search.java
@@ -14,39 +14,41 @@ class Main {
   private int serchIndex(int[] sortedArray, int targetNumber) {
 
     // ここから記述
+	  
     // 中間値のインデックス
     int middleIndex = 0;
     // 対象となる配列範囲の最小のインデックス
-	  int lowIndex = 0;
+　　int lowIndex = 0;
     // 対象となる配列範囲の最大のインデックス
-		int highIndex = sortedArray.length;
-		while (true) {
+　　int highIndex = sortedArray.length;
+　　while (true) {
     　// 中間値のインデックスを求める
     　// 対象配列の要素数が偶数である場合
       if ((highIndex-lowIndex+1) % 2 == 0) {
-        middleIndex = (highIndex - lowIndex + 1) / 2;
+	      middleIndex = (highIndex - lowIndex + 1) / 2;
       } else {
-      // 対象配列の要素数が奇数である場合
-			  middleIndex = (highIndex - lowIndex) / 2;
-			}
+      // 対象配列の要素数が奇数である場合 
+	      middleIndex = (highIndex - lowIndex) / 2;
+　　  }
       // 中間値と探索対象の値が一致した場合は、
       // 中間値のインデックスを返す。
-			if (targetNumber == sortedArray[middleIndex]) {
-			  return middleIndex;
-			} else if (targetNumber <= sortedArray[middleIndex]) {
-      　// もし中間値よりも探索対象の値が小さければ、
-        // 次の対象となる配列のインデックスの範囲は
-        // lowInde~middleIndexまでとなる。
-        // よって、最大のインデックスはmiddleIndexに変わる。
-			  highIndex = middleIndex;
-			} else {
+      if (targetNumber == sortedArray[middleIndex]) {
+	      return middleIndex;
+      } else if (targetNumber <= sortedArray[middleIndex]) {
+      // もし中間値よりも探索対象の値が小さければ、
+      // 次の対象となる配列のインデックスの範囲は
+      // lowInde~middleIndexまでとなる。
+      // よって、最大のインデックスはmiddleIndexに変わる。
+	      highIndex = middleIndex;
+      } else {
         // もし中間値よりも探索対象の値が大きければ、
         // 次の対象となる配列のインデックスの範囲は
         // middleIndex~highIndexまでとなる。
         // よって、最小のインデックスはmiddleIndexに変わる。
-			  lowIndex = middleIndex;
-			}
+	      lowIndex = middleIndex;
+      }
     }
+	  
     // ここまで記述
 
     // 探索対象が存在しない場合、-1を返却
